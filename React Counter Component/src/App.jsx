92960419e1fc02c20a8801f9043f65e91e0b4998
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-03-18 12:15:05 +0100

    Add files via upload

---
+++ React Counter Component/src/App.jsx
@@ -0,0 +1,9 @@
+// useState() hook = A react hook that allow the creation of a stateful variable
+// And a setter function to update its value in the virtual DOM [name, setName]
+import Counter from "./Counter.jsx";
+
+function App() {
+  return <Counter />;
+}
+
+export default App;
