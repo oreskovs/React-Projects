88ad3d7dface94cd67c5c90da3ad2fea8b00b79e
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-09-18 14:10:56 +0200

    Add files via upload

---
+++ ReactFacts/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+  return (
+    <header>
+      <nav className="navbar">
+        <img
+          src="src/assets/react_logo.png"
+          alt="React Logo"
+          className="nav-img"
+        />
+        <p className="header-title">ReactFacts</p>
+      </nav>
+    </header>
+  );
+}
